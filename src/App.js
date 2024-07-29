32b2704fa7e13a329b97b8d5fe18407065d1d39d
山本　太陽 <yamamototaiyou.811@gmail.com>
2024-07-29 23:37:07 +0900

    場所情報、天気情報の取得

--- src/App.js
+++ src/App.js
@@ -6,37 +6,52 @@ import Search from './components/Search';
 
 function App() {
   const [weater, setWeather] = useState('');
-  const [place, setPlace] = useState();
+  const [place, setPlace] = useState('東京');
+  const [lat, setLat] = useState('35.6828387');
+  const [lon, setLon] = useState('139.7594549');
+  const WEATHER_API_KEY = '91d22dc332a4b066cdd03ba5d6729121';
 
-  //位置情報
-  const getLocation = () => {
-    const WEATHER_API_KEY = '91d22dc332a4b066cdd03ba5d6729121';
+  /*位置情報*/
+  const fetchLocation = () => {
     return new Promise((resolve, reject) => {
       fetch(
-        `http://api.openweathermap.org/geo/1.0/direct?q=Tokyo,JP&appid=${WEATHER_API_KEY}`
+        //都市名とその緯度経度を取得
+        `http://api.openweathermap.org/geo/1.0/direct?q=Nagoya,JP&appid=${WEATHER_API_KEY}`
       )
         .then((res) => res.json())
         .then((data) => resolve(data));
     });
   };
 
+  /*位置情報をセット*/
   const loadLocal = async () => {
-    await getLocation();
+    let res = await fetchLocation();
+    console.log(res[0]);
+    setLat(res[0].lat);
+    setLon(res[0].lon);
+    setPlace(res[0].local_names.ja);
   };
-  //天気情報
-  // const weatherForecast = () => {
-  //   return new Promise((resolve, reject) => {
-  //     fetch(
-  //       `https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}`
-  //     )
-  //       .then((res) => res.json())
-  //       .then((data) => console.log(data));
-  //   });
-  // };
+
+  /*天気情報を取得*/
+  const fetchWeather = () => {
+    return new Promise((resolve, reject) => {
+      fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`
+      )
+        .then((res) => res.json())
+        .then((data) => resolve(data));
+    });
+  };
+
+  const loadWeather = async () => {
+    await fetchWeather();
+  };
 
   useEffect(() => {
     loadLocal();
+    loadWeather();
   }, []);
+
   // useEffect(() => {
   //   const fetchPreData = async () => {
   //     let data = await getAllPrefectures();
@@ -56,7 +71,7 @@ function App() {
   return (
     <div className="weather_app">
       <Search />
-      <Weather />
+      <Weather cityName={place} />
     </div>
   );
 }
