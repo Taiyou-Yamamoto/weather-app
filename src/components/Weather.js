5a06d1bc5257e1dbc7dc0a6f29dbb695ec31418c
山本　太陽 <yamamototaiyou.811@gmail.com>
2024-07-29 23:37:28 +0900

    スタイル

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import './Weather.css';
 
-const Weather = () => {
-  return <div>Weather</div>;
+const Weather = ({ lat, lon, cityName }) => {
+  return (
+    <div className="weather">
+      <h1 className="cityName">{cityName}</h1>
+    </div>
+  );
 };
 
 export default Weather;
