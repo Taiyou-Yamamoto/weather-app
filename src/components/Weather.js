9eb693d9e59aab3bcc8317f4fea3d377e3215d9a
山本　太陽 <yamamototaiyou.811@gmail.com>
2024-08-01 23:14:26 +0900

    div追加

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -4,8 +4,11 @@ import './Weather.css';
 const Weather = ({ lat, lon, prefecture, selectedCity, icon, main }) => {
   return (
     <div className="weather">
-      <h1 className="cityName">{prefecture}</h1>
-      <h1 className="cityName">{selectedCity}</h1>
+      <div className="local">
+        <h1 className="cityName">{prefecture}</h1>
+        <h1 className="cityName">{selectedCity}</h1>
+      </div>
+
       <img
         src={`http://openweathermap.org/img/wn/${icon}@2x.png`}
         // src={`http://openweathermap.org/img/wn/01d@2x.png`}
