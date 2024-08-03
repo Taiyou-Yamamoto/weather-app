f61c6ab081cbc13bf71ae224dbadecfeffcd90d0
山本　太陽 <yamamototaiyou.811@gmail.com>
2024-08-03 21:01:38 +0900

    スタイル

--- src/components/Weekly.js
+++ src/components/Weekly.js
@@ -1,15 +1,9 @@
 import React from 'react';
-import weatherIcon from '../components/Icons/100.svg';
 
 const Weekly = () => {
   return (
     <div className="Weekly">
-      <img
-        src={'weatherIcon'}
-        // src={`http://openweathermap.org/img/wn/${icon}@2x.png`}
-        // src={`http://openweathermap.org/img/wn/01d@2x.png`}
-        alt="Weather Icon"
-      ></img>
+      <img src={'./Icons/100.svg'} alt="Weather Icon"></img>
     </div>
   );
 };
