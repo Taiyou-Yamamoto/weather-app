645116697d49928f1413b4265f09612528850f13
山本　太陽 <yamamototaiyou.811@gmail.com>
2024-08-06 23:30:31 +0900

    Weeklyのスタイル

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,44 +5,45 @@ const Card = ({ day }) => {
   useEffect(() => {
     switch (day.weatherDescription) {
       case '晴れ':
-        setIcon('../../public/Icons/100.svg'); // 晴れのアイコンを設定
+        setIcon('/Icons/100.svg'); // 晴れのアイコンを設定
         break;
       case '晴れ時々曇り':
-        setIcon('../../public/Icons/100.svg'); // 晴れ時々曇りのアイコンを設定
+        setIcon('/Icons/101.svg'); // 晴れ時々曇りのアイコンを設定
         break;
       case '曇り時々晴れ':
-        setIcon('../../public/Icons/100.svg'); // 曇り時々晴れのアイコンを設定
+        setIcon('/Icons/201.svg'); // 曇り時々晴れのアイコンを設定
         break;
       case '曇り':
-        setIcon('../../public/Icons/100.svg'); // 曇りのアイコンを設定
+        setIcon('/Icons/200.svg'); // 曇りのアイコンを設定
         break;
       case '晴れ時々雨':
-        setIcon('../../public/Icons/100.svg'); // 晴れ時々雨のアイコンを設定
+        setIcon('/Icons/102.svg'); // 晴れ時々雨のアイコンを設定
         break;
       case '雨のち晴れ':
-        setIcon('../../public/Icons/100.svg'); // 雨のち晴れのアイコンを設定
+        setIcon('/Icons/301.svg'); // 雨のち晴れのアイコンを設定
         break;
       case '雨':
-        setIcon('../../public/Icons/100.svg'); // 雨のアイコンを設定
+        setIcon('/Icons/300.svg'); // 雨のアイコンを設定
         break;
       case '曇り時々雨':
-        setIcon('../../public/Icons/100.svg'); // 曇り時々雨のアイコンを設定
+        setIcon('/Icons/202.svg'); // 曇り時々雨のアイコンを設定
         break;
       case '雨のち曇り':
-        setIcon('../../public/Icons/100.svg'); // 雨のち曇りのアイコンを設定
+        setIcon('/Icons/302.svg'); // 雨のち曇りのアイコンを設定
         break;
       default:
         setIcon(''); // 天気情報なしのアイコンを設定
         break;
     }
+    console.log('ええ', day.weatherDescription);
   }, [day.weatherDescription]);
   return (
     <div className="Card">
       <div className="date">{day.date}</div>
       <div className="icon"></div>
-      <div className="">{icon}</div>
-      <div className="min">{Math.round(day.min - 273.15)}</div>
-      <div className="max">{Math.round(day.max - 273.15)}</div>
+      <img src={icon} alt="Weather Icon" />
+      <div className="weatherDescription">{day.weatherDescription}</div>
+      <div className="min">{Math.round(day.min - 273.15)}°/ {Math.round(day.max - 273.15)}°</div>
     </div>
   );
 };
